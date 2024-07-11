ab3f09b0566add76f6a2fb7b81cd8ce61053ca0f
Daniel Alvaro <danielkisaran@gmail.com>
2024-07-12 04:04:40 +0700

    implementation view and API in front-end side

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,113 +1,131 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from 'react';
+import { ScheduleComponent, Day, Week, Month, Agenda, Inject, ViewDirective, ViewsDirective, WorkWeek } from '@syncfusion/ej2-react-schedule';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCdkx3RXxbf1x0ZFNMYV9bRHBPMyBoS35RckVkWH5ednVWRmdVVU13');
+const formatDate = (dateString) => {
+  const options = { day: '2-digit', weekday: 'short' };
+  return new Date(dateString).toLocaleDateString('en-US', options);
+};
+
+const formatTime = (dateString) => {
+  const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+  return new Date(dateString).toLocaleTimeString('en-US', options);
+};
 
 export default function Home() {
+  const [display, setDisplay] = useState(true);
+  const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/events')
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data);
+        setFilteredEvents(data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching events:', error);
+      });
+  }, []);
+
+  const handleActionComplete = (args) => {
+    if (args.requestType === 'eventCreated') {
+      fetch('http://localhost:3000/events', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(args.data[0]),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error('Error adding event:', error);
+        });
+    } else if (args.requestType === 'eventChanged') {
+      const eventId = args.data[0].Id;
+      const apiUrl = `http://localhost:3000/events/${eventId}`;
+      fetch(apiUrl, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(args.data[0]),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error('Error updating event:', error);
+        });
+    } else if (args.requestType === 'eventRemoved') {
+      const eventId = args.data[0].Id;
+      const apiUrl = `http://localhost:3000/events/${eventId}`;
+      fetch(apiUrl, {
+        method: 'DELETE',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error('Error removing event:', error);
+        });
+    }
+  };
+
+  const handleSearchClick = (e) => {
+    if (e.target.value === '') {
+      setDisplay(true);
+    } else {
+      setDisplay(false);
+      const filtered = events.data.filter((event) => {
+        const subjectMatch = event.Subject.toLowerCase().includes(e.target.value.toLowerCase());
+        const startTimeString = new Date(event.StartTime).toLocaleString().toLowerCase();
+        const dayString = new Date(event.StartTime).toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase();
+        const startTimeMatch = startTimeString.includes(e.target.value) || dayString.includes(e.target.value);
+        return subjectMatch || startTimeMatch;
+      });
+      setFilteredEvents(filtered);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full">
+      <input className="w-1/3 border-2 my-2 ml-2 p-2 rounded-md focus:outline-none text-slate-500 lg:w-1/4" type="text" placeholder="Enter keyword.." onKeyUp={handleSearchClick} />
+      {display && (
+        <div className="mx-2 shadow-md">
+          <ScheduleComponent eventSettings={{ dataSource: events.data }} currentView="Month" actionComplete={handleActionComplete}>
+            <Inject services={[Day, Week, Month, WorkWeek, Agenda]} />
+          </ScheduleComponent>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
+      {!display && (
+        <div>
+          <div className="w-full flex items-center gap-2 mx-2">
+            {filteredEvents.map((item) => (
+              <div key={item.Id} className="w-[46%] h-36 flex gap-0 items-center rounded-md shadow-md lg:w-[20%] lg:h-32 lg:gap-2">
+                <div className="ml-2">
+                  <p>{formatDate(item.StartTime)}</p>
+                </div>
+                <div className="border-l-4 border-blue-900 pl-2 py-2">
+                  <h3 className="text- font-medium">{item.Subject}</h3>
+                  <p className="text-sm text-slate-500">
+                    {formatTime(item.StartTime)} - {formatTime(item.EndTime)}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </main>
   );
 }
